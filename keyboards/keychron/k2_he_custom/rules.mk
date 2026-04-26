57187da23aa4eee715d05ca69a97c5049eed68af
afiqarif <akamarulzaidi@gmail.com>
2026-04-26 12:45:56 +0800

    update

--- keyboards/keychron/k2_he_custom/rules.mk
+++ keyboards/keychron/k2_he_custom/rules.mk
@@ -8,9 +8,9 @@ USE_FPU = yes
 OPT_DEFS += -DSHARED_EP_ENABLE -DKEYBOARD_SHARED_EP
 OPT_DEFS += -DXINPUT_ENABLE
 
-include keyboards/keychron/k2_he_custom/custom/analog_matrix/analog_matrix.mk
-include keyboards/keychron/k2_he_custom/custom/keychron_common.mk
-include keyboards/keychron/k2_he_custom/custom/wireless/wireless.mk
+include keyboards/keychron/k2_he_custom/src/analog_matrix/analog_matrix.mk
+include keyboards/keychron/k2_he_custom/src/keychron_common.mk
+include keyboards/keychron/k2_he_custom/src/wireless/wireless.mk
 
 VPATH += $(TOP_DIR)/keyboards/keychron
 
